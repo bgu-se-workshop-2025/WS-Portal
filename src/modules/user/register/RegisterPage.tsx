006e80c72de61e9fd304bed029a4dcb6a293fc7a
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-09 13:29:16 +0300

    finalized page, missing functionality

--- src/modules/user/register/RegisterPage.tsx
+++ src/modules/user/register/RegisterPage.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import PaneLayout from "./components/PaneLayout/PaneLayout"
 import RegisterPageResources from "./RegisterPageResources.json"
 import { Button, Typography } from "@mui/material";
+import FormTextField from "./components/FormTextField/FormTextField";
+import { isValidEmailAddress, isValidPassword, isValidStringProperty, isValidUsername } from "./utils/formValidationsUtil";
 
 const RegisterPage = () => {
     const navigate = useNavigate();
@@ -10,12 +12,43 @@ const RegisterPage = () => {
         navigate("/login");
     }
 
+    const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const firstName = formData.get(RegisterPageResources.Main.Form.FirstName.Id);
+        const lastName = formData.get(RegisterPageResources.Main.Form.LastName.Id);
+        const username = formData.get(RegisterPageResources.Main.Form.Username.Id);
+        const email = formData.get(RegisterPageResources.Main.Form.Email.Id);
+        const password = formData.get(RegisterPageResources.Main.Form.Password.Id);
+
+        const valid = (
+            Array.from(formData.values()).reduce((acc, value) => acc && typeof (value) === "string", true) &&
+            isValidStringProperty(firstName as string) &&
+            isValidStringProperty(lastName as string) &&
+            isValidUsername(username as string) &&
+            isValidEmailAddress(email as string) &&
+            isValidStringProperty(password as string)
+
+        );
+
+        if (!valid) {
+            return
+        }
+
+        event.currentTarget.reset();
+    }
+
     return (
         <PaneLayout.Container>
             <PaneLayout.Pane>
-                <Typography variant="h2">{RegisterPageResources.SidePanel.Title}</Typography>
+                <Typography
+                    variant="h2"
+                    sx={RegisterPageResources.Styles.MarginBottom}
+                >
+                    {RegisterPageResources.SidePanel.Title}
+                </Typography>
                 <Typography variant="body1">
-
+                    {RegisterPageResources.SidePanel.Body}
                 </Typography>
                 <Button
                     onClick={handleLogin}
@@ -26,7 +59,56 @@ const RegisterPage = () => {
                 </Button>
             </PaneLayout.Pane>
             <PaneLayout.Pane main>
-                <Typography variant="h2">Register a new user</Typography>
+                <Typography
+                    variant="h2"
+                    sx={RegisterPageResources.Styles.MarginBottom}
+                >
+                    {RegisterPageResources.Main.Title}
+                </Typography>
+                <form onSubmit={handleRegister}>
+                    <FormTextField
+                        id={RegisterPageResources.Main.Form.FirstName.Id}
+                        label={RegisterPageResources.Main.Form.FirstName.Label}
+                        placeholder={RegisterPageResources.Main.Form.FirstName.Placeholder}
+                        required
+                        validation={isValidStringProperty}
+                    />
+                    <FormTextField
+                        id={RegisterPageResources.Main.Form.LastName.Id}
+                        label={RegisterPageResources.Main.Form.LastName.Label}
+                        placeholder={RegisterPageResources.Main.Form.LastName.Placeholder}
+                        required
+                        validation={isValidStringProperty}
+                    />
+                    <FormTextField
+                        id={RegisterPageResources.Main.Form.Username.Id}
+                        label={RegisterPageResources.Main.Form.Username.Label}
+                        placeholder={RegisterPageResources.Main.Form.Username.Placeholder}
+                        required
+                        validation={isValidUsername}
+                        infoPop={RegisterPageResources.Main.Form.Username.Info}
+                    />
+                    <FormTextField
+                        id={RegisterPageResources.Main.Form.Email.Id}
+                        label={RegisterPageResources.Main.Form.Email.Label}
+                        placeholder={RegisterPageResources.Main.Form.Email.Placeholder}
+                        required
+                        validation={isValidEmailAddress}
+                        infoPop={RegisterPageResources.Main.Form.Email.Info}
+                    />
+                    <FormTextField
+                        id={RegisterPageResources.Main.Form.Password.Id}
+                        label={RegisterPageResources.Main.Form.Password.Label}
+                        placeholder={RegisterPageResources.Main.Form.Password.Placeholder}
+                        type="password"
+                        required
+                        validation={isValidPassword}
+                        infoPop={RegisterPageResources.Main.Form.Password.Info}
+                    />
+                    <Button variant="contained" type="submit" sx={RegisterPageResources.Styles.Button}>
+                        {RegisterPageResources.Main.Form.Button.Content}
+                    </Button>
+                </form>
             </PaneLayout.Pane>
         </PaneLayout.Container>
     );
