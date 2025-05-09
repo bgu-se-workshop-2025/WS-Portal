1c172914a38c64dd0a6d58181cd92d7c62cc23b9
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-09 11:44:22 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./modules/user/login/LoginPage";
+import RegisterPage from "./modules/user/register/RegisterPage";
 
 const rootElement = document.getElementById("App") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<></>} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element={<></>} />
       </Routes>
     </BrowserRouter>
