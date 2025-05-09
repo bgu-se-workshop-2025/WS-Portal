2669437ed98a95b1e48f6d772ea8c994b9824ab8
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-09 13:04:55 +0300

    change the primary color

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./modules/user/login/LoginPage";
 import RegisterPage from "./modules/user/register/RegisterPage";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const rootElement = document.getElementById("App") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<></>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<></>} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
