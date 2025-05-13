93dfd6352989eb1da511337e7181ed6e52963807
ShalevKk <kayatsha@post.bgu.ac.il>
2025-05-13 16:05:22 +0300

    Finished implement publicOrder SDK

--- src/sdk/modules/order/publicOrder.ts
+++ src/sdk/modules/order/publicOrder.ts
@@ -1,51 +1,7 @@
 import { SDK } from '../../sdk';
+import { UserOrderDto, OrderRequestDetails } from "../../../shared/types/dtos.ts";
 
 const publicOrder = "public/order";
-
-export interface UserOrderDto {
-    id: string;
-    time: string;
-    buyerId: string;
-    // TODO ShippingAdderss
-    cartSnapshot: string;
-}
-
-export interface OrderRequestDetails {
-    cart: CartDto;
-    paymentDetails: PaymentDetails;
-    // TODO shippingAddress: ShippingAddress;
-}
-
-export interface CartDto {
-    ownerId: string;
-    stores: CartStoreBasketDto[];
-}
-
-export interface CartStoreBasketDto {
-    storeId: string;
-    products: CartProductEntryDto[];
-}
-
-export interface CartProductEntryDto {
-    productId: string;
-    quantity: number;
-}
-
-export interface PaymentDetails {
-    paymentMethod: PaymentMethod;
-    externalId: string;
-    payerEmail: string;
-    payerId: string;
-}
-
-export enum PaymentMethod {
-    CREDIT_CARD = 0,
-    PAYPAL = 1,
-    APPLE_PAY = 2,
-    GOOGLE_PAY = 3,
-}
-
-
 
 export async function createOrder(this: SDK, request: OrderRequestDetails): Promise<UserOrderDto> {
     const response = await this.post(`${publicOrder}`, request);
