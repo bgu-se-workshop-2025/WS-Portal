e8007f068747a3c6dc2e5516048a7faa2b88563c
LiorJbgu <jermin@post.bgu.ac.il>
2025-05-13 12:19:27 +0300

    fix

--- src/sdk/modules/store/review.ts
+++ src/sdk/modules/store/review.ts
@@ -1,30 +1,22 @@
+import { ReviewDto } from "../../../shared/types/dtos.ts";
 import { SDK } from "../../sdk.ts";
 
 const storeController = "store";
 const productController = "product";
-export interface Review {
-    id: string;
-    productId: string;  
-    storeId: string;
-    writerId: string;
-    title: string;
-    body: string;
-    rating: number;
-    date: string;
-}
 
-export async function createStoreReview(this: SDK, payload: Review): Promise<Review> {
+
+export async function createStoreReview(this: SDK, payload: ReviewDto): Promise<ReviewDto> {
     const response = await this.post(`${storeController}/${payload.storeId}/`, payload);
 
     if (!response.ok) {
         const err = await response.text();
         throw new Error(`createStoreReview failed: ${err}`);
     }
-    const result = (await response.json()) as Review;
+    const result = (await response.json()) as ReviewDto;
     return result;
 }
 
-export async function createProductReview(this: SDK, payload: Review): Promise<Review> {
+export async function createProductReview(this: SDK, payload: ReviewDto): Promise<ReviewDto> {
     const response = await this.post(
         `${storeController}/${payload.storeId}/${productController}/${payload.productId}`,
         payload
@@ -34,7 +26,7 @@ export async function createProductReview(this: SDK, payload: Review): Promise<R
         const err = await response.text();
         throw new Error(`createProductReview failed: ${err}`);
     }
-    const result = (await response.json()) as Review;
+    const result = (await response.json()) as ReviewDto;
     return result;
 }
 
