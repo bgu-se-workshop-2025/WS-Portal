16804d552d3d91bd1127fcd25eb3ae46e352c139
ShalevKk <kayatsha@post.bgu.ac.il>
2025-05-13 15:57:27 +0300

    Added Dtos

--- src/sdk/modules/order/publicOrder.ts
+++ src/sdk/modules/order/publicOrder.ts
@@ -11,10 +11,41 @@ export interface UserOrderDto {
 }
 
 export interface OrderRequestDetails {
-    // TODO cart: CartDto;
-    // TODO paymentDatails: PaymentDetails;
+    cart: CartDto;
+    paymentDetails: PaymentDetails;
     // TODO shippingAddress: ShippingAddress;
 }
+
+export interface CartDto {
+    ownerId: string;
+    stores: CartStoreBasketDto[];
+}
+
+export interface CartStoreBasketDto {
+    storeId: string;
+    products: CartProductEntryDto[];
+}
+
+export interface CartProductEntryDto {
+    productId: string;
+    quantity: number;
+}
+
+export interface PaymentDetails {
+    paymentMethod: PaymentMethod;
+    externalId: string;
+    payerEmail: string;
+    payerId: string;
+}
+
+export enum PaymentMethod {
+    CREDIT_CARD = 0,
+    PAYPAL = 1,
+    APPLE_PAY = 2,
+    GOOGLE_PAY = 3,
+}
+
+
 
 export async function createOrder(this: SDK, request: OrderRequestDetails): Promise<UserOrderDto> {
     const response = await this.post(`${publicOrder}`, request);
