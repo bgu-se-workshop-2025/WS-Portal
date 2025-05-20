29b363327970bbb5c8f02d281f9b763a4a086b21
ShalevKk <kayatsha@post.bgu.ac.il>
2025-05-20 19:35:12 +0300

    initial commit

---
+++ src/shared/components/Transaction/TransactionList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Box,
+  Typography,
+  Paper,
+  Stack,
+  IconButton,
+} from "@mui/material";
+
+interface TransactionListProps {
+
+}
+
+const TransactionList: React.FC<TransactionListProps> = ({
+
+}) => {
+    return (
+
+    );
+};
+
+export default TransactionList;
---
+++ src/shared/components/Transaction/TransatcionView.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Box,
+  Typography,
+  Paper,
+  Stack,
+  IconButton,
+} from "@mui/material";
+
+interface TransactionViewProps {
+
+}
+
+const TransactionView: React.FC<TransactionViewProps> = ({
+
+}) => {
+    return (
+
+    );
+};
+
+export default TransactionView;
