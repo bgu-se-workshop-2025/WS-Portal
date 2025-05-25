1104d03e8a48f02e9102fcf8a85f89eb176ec240
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-25 21:04:55 +0300

    refactor to resources

--- src/modules/store/components/subpages/discounts/StoreDiscountEditor/StoreDiscountEditor.tsx
+++ src/modules/store/components/subpages/discounts/StoreDiscountEditor/StoreDiscountEditor.tsx
@@ -2,29 +2,49 @@ import { Box, Button, Dialog, Stack, Typography } from "@mui/material";
 import { DiscountDataModel } from "../DiscountTypes";
 import { useState } from "react";
 import { Close } from "@mui/icons-material";
+import StoreDiscountEditorResources from "./StoreDiscountEditorResources.json";
 import DiscountTypeSelector from "./components/DiscountTypeSelector";
 
 export type StoreDiscountEditorProps = {
     storeId?: string;
     productId?: string;
     discountId?: string;
-    discountState: DiscountDataModel;
+    discountState?: DiscountDataModel;
 }
 
 
-const StoreDiscountEditor = () => {
+const StoreDiscountEditor = ({
+    storeId,
+    productId,
+    discountId,
+    discountState
+}: StoreDiscountEditorProps) => {
     const [open, setOpen] = useState<boolean>(true);
+    const [policy, setPolicy] = useState<DiscountDataModel | undefined>(discountState);
+
+    const onSave = () => {
+        if (!policy) {
+            console.error("No discount policy set.");
+            return;
+        }
+        // Implement save logic here
+        console.log("Saving discount policy:", policy);
+        setOpen(false);
+    }
+
     return (<Dialog open={open}>
         <Box width="24vw" height="64vh" padding="1rem">
-            <Stack direction={"row"} justifyContent={"flex-end"}>
-                <Button onClick={() => setOpen(!open)}><Close /></Button>
+            <Stack direction="row" justifyContent="flex-end">
+                <Button onClick={() => setOpen(!open)}>
+                    <Close />
+                </Button>
             </Stack>
             <Stack justifyContent="space-between" height="90%">
                 <Stack gap={2}>
-                    <Typography variant="h5">Discount Editor</Typography>
-                    <DiscountTypeSelector />
+                    <Typography variant="h5">{StoreDiscountEditorResources.DiscountEditorTitle}</Typography>
+                    {!policy && <DiscountTypeSelector setPolicy={setPolicy} />}
                 </Stack>
-                <Button>Save Discount</Button>
+                <Button onClick={onSave}>{StoreDiscountEditorResources.SaveDiscountButton}</Button>
             </Stack>
         </Box>
     </Dialog>);
