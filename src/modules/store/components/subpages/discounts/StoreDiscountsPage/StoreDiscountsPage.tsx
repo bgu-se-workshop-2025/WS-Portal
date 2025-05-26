8560dd4c28d4954eca4abdf9f6406cbb8cc12fca
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-26 17:34:59 +0300

    add loading etc

--- src/modules/store/components/subpages/discounts/StoreDiscountsPage/StoreDiscountsPage.tsx
+++ src/modules/store/components/subpages/discounts/StoreDiscountsPage/StoreDiscountsPage.tsx
@@ -1,8 +1,7 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, CircularProgress, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import { DiscountDataModel } from "../DiscountTypes";
 import PaginatedDiscountsTable from "./components/PaginatedDiscountsTable";
-import { discountTestData } from "../DiscountTestData";
 import { Add, Loyalty } from "@mui/icons-material";
 
 const MainContainerProps = {
@@ -19,13 +18,36 @@ const CommandBar = () => {
 }
 
 const StoreDiscountsPage = () => {
-	const [discounts, setDiscounts] = useState<DiscountDataModel[]>(discountTestData);
+	const [discounts, setDiscounts] = useState<DiscountDataModel[]>([]); // todo initialize with actual data from the store
+	const [loading, setLoading] = useState(true);
+
+	useState(() => {
+		if (false) { // todo replace with actual condition to fetch discounts
+			setDiscounts([]);
+		}
+		setTimeout(() => { setLoading(false); }, 2000); // Simulate a delay for loading
+	},);
 
 	return (
 		<Stack sx={MainContainerProps}>
 			<Typography variant="h4">Store Discounts <Loyalty /></Typography>
 			<CommandBar />
-			<PaginatedDiscountsTable rows={discounts} />
+			{loading &&
+				<Stack direction="row" justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
+					<CircularProgress />
+				</Stack>
+			}
+			{!loading && discounts.length === 0 &&
+				<Stack>
+					<PaginatedDiscountsTable rows={discounts} />
+					<Stack direction="row" justifyContent="center" alignItems="center" sx={{ height: '100%' }} padding={2}>
+						<Typography variant="body1" color="textSecondary">No discounts available.</Typography>
+					</Stack>
+				</Stack>
+			}
+			{!loading && discounts.length > 0 &&
+				<PaginatedDiscountsTable rows={discounts} />
+			}
 		</Stack>
 	);
 };
