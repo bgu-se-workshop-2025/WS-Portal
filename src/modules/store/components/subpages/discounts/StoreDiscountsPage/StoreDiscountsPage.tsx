d3df0a42516495007cf806c11e8be07fb587095f
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-26 17:27:29 +0300

    cmd bar

--- src/modules/store/components/subpages/discounts/StoreDiscountsPage/StoreDiscountsPage.tsx
+++ src/modules/store/components/subpages/discounts/StoreDiscountsPage/StoreDiscountsPage.tsx
@@ -1,25 +1,33 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import { DiscountDataModel } from "../DiscountTypes";
 import PaginatedDiscountsTable from "./components/PaginatedDiscountsTable";
+import { discountTestData } from "../DiscountTestData";
+import { Add, Loyalty } from "@mui/icons-material";
 
 const MainContainerProps = {
 	display: "flex",
 	gap: 2,
 	padding: 4,
 	paddingLeft: 8,
+};
+
+const CommandBar = () => {
+	return <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
+		<Button><Add />Create New Discount</Button>
+	</Stack>
 }
 
 const StoreDiscountsPage = () => {
-	const [discounts, setDiscounts] = useState<DiscountDataModel[]>([]);
+	const [discounts, setDiscounts] = useState<DiscountDataModel[]>(discountTestData);
+
 	return (
-		<Stack {...MainContainerProps}>
-			<Typography variant="h4" fontWeight="bold">
-				Store Discounts
-			</Typography>
-			<PaginatedDiscountsTable rowsPerPage={25} rows={discounts} />
+		<Stack sx={MainContainerProps}>
+			<Typography variant="h4">Store Discounts <Loyalty /></Typography>
+			<CommandBar />
+			<PaginatedDiscountsTable rows={discounts} />
 		</Stack>
 	);
-}
+};
 
 export default StoreDiscountsPage;
