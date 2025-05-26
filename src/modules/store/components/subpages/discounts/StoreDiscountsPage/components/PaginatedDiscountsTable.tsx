3d464c03b2260b2a75197d1adc4907e554b0b58a
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-26 17:27:20 +0300

    add delete button

--- src/modules/store/components/subpages/discounts/StoreDiscountsPage/components/PaginatedDiscountsTable.tsx
+++ src/modules/store/components/subpages/discounts/StoreDiscountsPage/components/PaginatedDiscountsTable.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,18 +6,33 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { DiscountDataModel } from '../../DiscountTypes';
+import { getLabelForTag, isCategoryScoped } from '../../util/discountUtils';
+import { Button } from '@mui/material';
+import { Delete } from '@mui/icons-material';
 
 export type PaginatedDiscountTableProps = {
-    rowsPerPage: number;
     rows: DiscountDataModel[];
 }
 
-const PaginatedDiscountsTable = ({ rowsPerPage, rows }: PaginatedDiscountTableProps) => {
+const PaginatedDiscountsTable = ({ rows }: PaginatedDiscountTableProps) => {
+
+    const deleteDiscount = (id?: string) => {
+        if (!id) {
+            console.error("No discount ID provided for deletion.");
+            return;
+        }
+        console.log(`Delete discount with id: ${id}`);
+        // Implement the delete logic here, e.g., call an API to delete the discount
+    };
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
+                        <TableCell align='center' width={2}>
+                            <Delete />
+                        </TableCell>
                         <TableCell align="left">Title</TableCell>
                         <TableCell align="left">Description</TableCell>
                         <TableCell align="left">Percentage</TableCell>
@@ -29,11 +43,25 @@ const PaginatedDiscountsTable = ({ rowsPerPage, rows }: PaginatedDiscountTablePr
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow key={row.id}>
+                            <TableCell width={2}>
+                                <Button onClick={() => deleteDiscount(row.id)} size="small">
+                                    <Delete />
+                                </Button>
+                            </TableCell>
                             <TableCell align="left">{row.title}</TableCell>
                             <TableCell align="left">{row.description}</TableCell>
                             <TableCell align="left">{row.discountPercentage}%</TableCell>
-                            <TableCell align="left">{row.scope}</TableCell>
-                            <TableCell align="left">{row.type}</TableCell>
+                            <TableCell align="left">{
+                                isCategoryScoped(row)
+                                    ? "Category"
+                                    : row.scope ?
+                                        row.scope.storeId
+                                            ? "Store"
+                                            : "Product"
+                                        : ""
+                            }
+                            </TableCell>
+                            <TableCell align="left">{getLabelForTag(row.type)}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
