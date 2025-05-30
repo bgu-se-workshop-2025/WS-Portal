9f3fde84c11f7d56a15f17c91af08bb32594a89f
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-30 14:14:09 +0300

    format and add the functions required

--- src/sdk/sdk.ts
+++ src/sdk/sdk.ts
@@ -23,128 +23,205 @@ import * as requests from "../shared/types/requests";
 import * as responses from "../shared/types/responses";
 
 interface SDKOptions {
-  baseUrl: string;
+    baseUrl: string;
 }
 
 export class SDK {
+    // Auth SDK
+    public login!: (
+        payload: requests.LoginUserRequest
+    ) => Promise<responses.GeneralAuthResponse>;
+    public register!: (
+        payload: requests.RegisterUserRequest
+    ) => Promise<responses.GeneralAuthResponse>;
+    public updatePublicUserProfileDetails!: (
+        id: string,
+        payload: dtos.UpdatePublicUserDto
+    ) => Promise<dtos.PublicUserDto>;
+    // Message SDK
+    public createMessage!: (
+        payload: dtos.MessageDto
+    ) => Promise<dtos.MessageDto>;
+    public getMessages!: (
+        page?: number,
+        size?: number
+    ) => Promise<dtos.MessageDto[]>;
+    public getSentMessages!: (
+        page?: number,
+        size?: number
+    ) => Promise<dtos.MessageDto[]>;
+    public getMessageById!: (messageId: string) => Promise<dtos.MessageDto>;
+    public updateMessage!: (
+        messageId: string,
+        payload: dtos.MessageDto
+    ) => Promise<dtos.MessageDto>;
+    public deleteMessage!: (messageId: string) => Promise<void>;
+    // Product SDK
+    public getProduct!: (id: string) => Promise<dtos.ProductDto>;
+    public getProducts!: (
+        payload: requests.GetProductsPayload
+    ) => Promise<dtos.ProductDto[]>;
+    // Review SDK
+    public createStoreReview!: (
+        payload: dtos.ReviewDto
+    ) => Promise<dtos.ReviewDto>;
+    public createProductReview!: (
+        payload: dtos.ReviewDto
+    ) => Promise<dtos.ReviewDto>;
+    //Store SDK
+    public createStore!: (payload: dtos.StoreDto) => Promise<dtos.StoreDto>;
+    public updateStore!: (
+        storeId: string,
+        payload: dtos.StoreDto
+    ) => Promise<dtos.StoreDto>;
+    public createProduct!: (
+        storeId: string,
+        payload: dtos.ProductDto
+    ) => Promise<dtos.ProductDto>;
+    public updateProduct!: (
+        storeId: string,
+        productId: string,
+        payload: dtos.ProductDto
+    ) => Promise<dtos.ProductDto>;
+    public deleteProduct!: (
+        storeId: string,
+        productId: string
+    ) => Promise<void>;
+    public addSeller!: (
+        storeId: string,
+        payload: dtos.SellerDto
+    ) => Promise<dtos.SellerDto>;
+    public removeSeller!: (storeId: string, sellerId: string) => Promise<void>;
+    //Public Store SDK
+    public getStore!: (id: string) => Promise<dtos.StoreDto>;
+    public getStores!: (pageable: dtos.Pageable) => Promise<dtos.StoreDto[]>;
+    public getStoreOfficials!: (
+        storeId: string
+    ) => Promise<dtos.PublicUserDto[]>;
+    //Order SDK
+    public getUserOrders!: (
+        payload: dtos.Pageable
+    ) => Promise<dtos.UserOrderDto[]>;
+    public getUserOrderById!: (id: string) => Promise<dtos.UserOrderDto>;
+    public getStoreOrderById!: (
+        orderId: string,
+        storeId: string
+    ) => Promise<dtos.StoreOrderDto>;
+    public getStoreOrders!: (
+        storeId: string,
+        payload: dtos.Pageable
+    ) => Promise<dtos.StoreOrderDto[]>;
+    //Public Order SDK
+    public createOrder!: (
+        payload: dtos.OrderRequestDetails
+    ) => Promise<dtos.UserOrderDto>;
+    // Public Review SDK
+    public getStoreReviews!: (
+        storeId: string,
+        page?: number,
+        size?: number
+    ) => Promise<dtos.ReviewDto[]>;
+    public getProductReviews!: (
+        storeId: string,
+        productId: string,
+        page?: number,
+        size?: number
+    ) => Promise<dtos.ReviewDto[]>;
+    // Admin SDK
+    public suspendUser!: (username: string, millis: number) => Promise<string>;
+    public cancelSuspensionUser!: (username: string) => Promise<void>;
+    public getSuspensions!: (
+        page: number,
+        limit: number
+    ) => Promise<dtos.SuspensionTicketDto[]>;
+    public elevateUser!: (username: string) => Promise<string>;
+    public isAdmin!: () => Promise<dtos.AdminDetailsDto>;
+    // Cart SDK
+    public getCart!: () => Promise<dtos.CartDto>;
+    public addProductToCart!: (
+        productId: number,
+        payload: { quantity: number }
+    ) => Promise<dtos.CartDto>;
+    public removeProductFromCart!: (productId: number) => Promise<void>;
+    public updateProductInCart!: (
+        productId: number,
+        payload: { quantity: number }
+    ) => Promise<dtos.CartDto>;
 
-  // Auth SDK
-  public login!: (payload: requests.LoginUserRequest) => Promise<responses.GeneralAuthResponse>;
-  public register!: (payload: requests.RegisterUserRequest) => Promise<responses.GeneralAuthResponse>;
-  public updatePublicUserProfileDetails!: (id: string, payload: dtos.UpdatePublicUserDto) => Promise<dtos.PublicUserDto>;
-  // Message SDK
-  public createMessage!: (payload: dtos.MessageDto) => Promise<dtos.MessageDto>;
-  public getMessages!: (page?: number, size?: number) => Promise<dtos.MessageDto[]>;
-  public getSentMessages!: (page?: number, size?: number) => Promise<dtos.MessageDto[]>;
-  public getMessageById!: (messageId: string) => Promise<dtos.MessageDto>;
-  public updateMessage!: (messageId: string, payload: dtos.MessageDto) => Promise<dtos.MessageDto>;
-  public deleteMessage!: (messageId: string) => Promise<void>;
-  // Product SDK
-  public getProduct!: (id: string) => Promise<dtos.ProductDto>;
-  public getProducts!: (payload: requests.GetProductsPayload) => Promise<dtos.ProductDto[]>;
-  // Review SDK
-  public createStoreReview!: (payload: dtos.ReviewDto) => Promise<dtos.ReviewDto>;
-  public createProductReview!: (payload: dtos.ReviewDto) => Promise<dtos.ReviewDto>;
-  //Store SDK
-  public createStore!: (payload: dtos.StoreDto) => Promise<dtos.StoreDto>;
-  public updateStore!: (storeId: string, payload: dtos.StoreDto) => Promise<dtos.StoreDto>;
-  public createProduct!: (storeId: string, payload: dtos.ProductDto) => Promise<dtos.ProductDto>;
-  public updateProduct!: (storeId: string, productId: string, payload: dtos.ProductDto) => Promise<dtos.ProductDto>;
-  public deleteProduct!: (storeId: string, productId: string) => Promise<void>;
-  public addSeller!: (storeId: string, payload: dtos.SellerDto) => Promise<dtos.SellerDto>;
-  public removeSeller!: (storeId: string, sellerId: string) => Promise<void>;
-  //Public Store SDK
-  public getStore!: (id: string) => Promise<dtos.StoreDto>;
-  public getStores!: (pageable: dtos.Pageable) => Promise<dtos.StoreDto[]>;
-  public getStoreOfficials!: (storeId: string) => Promise<dtos.PublicUserDto[]>;
-  //Order SDK
-  public getUserOrders!: (payload: dtos.Pageable) => Promise<dtos.UserOrderDto[]>;
-  public getUserOrderById!: (id: string) => Promise<dtos.UserOrderDto>;
-  public getStoreOrderById!: (orderId: string, storeId: string) => Promise<dtos.StoreOrderDto>;
-  public getStoreOrders!: (storeId: string, payload: dtos.Pageable) => Promise<dtos.StoreOrderDto[]>;
-  //Public Order SDK
-  public createOrder!: (payload: dtos.OrderRequestDetails) => Promise<dtos.UserOrderDto>;
-  // Public Review SDK
-  public getStoreReviews!: (storeId: string, page?: number, size?: number) => Promise<dtos.ReviewDto[]>;
-  public getProductReviews!: (storeId: string, productId: string, page?: number, size?: number) => Promise<dtos.ReviewDto[]>;
-  // Admin SDK
-  public suspendUser!: (userId: string, millis: number) => Promise<string>;
-  public elevateUser!: (userId: string) => Promise<string>;
-  public isAdmin!: () => Promise<dtos.AdminDetailsDto>;
-  // Cart SDK
-  public getCart!: () => Promise<dtos.CartDto>;
-  public addProductToCart!: (productId: number, payload: { quantity: number }) => Promise<dtos.CartDto>;
-  public removeProductFromCart!: (productId: number) => Promise<void>;
-  public updateProductInCart!: (productId: number, payload: { quantity: number }) => Promise<dtos.CartDto>;
+    private options: SDKOptions;
 
-  private options: SDKOptions;
+    constructor(options: SDKOptions) {
+        this.options = options;
 
-  constructor(options: SDKOptions) {
-    this.options = options;
+        Object.assign(this, {
+            ...auth,
 
-    Object.assign(this, {
-      ...auth,
+            ...order,
+            ...publicOrder,
 
-      ...order,
-      ...publicOrder,
+            ...policy,
+            ...product,
+            ...publicReview,
+            ...publicStore,
+            ...review,
+            ...store,
 
-      ...policy,
-      ...product,
-      ...publicReview,
-      ...publicStore,
-      ...review,
-      ...store,
+            ...admin,
+            ...cart,
+            ...message,
+            ...publicUser,
+            ...user,
+        });
+    }
 
-      ...admin,
-      ...cart,
-      ...message,
-      ...publicUser,
-      ...user,
-    });
-  }
+    private getHeaders(): Record<string, string> {
+        const token = TokenService.token;
+        return {
+            "Content-Type": "application/json",
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
+        };
+    }
 
-  private getHeaders(): Record<string, string> {
-    const token = TokenService.token;
-    return {
-      "Content-Type": "application/json",
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
-    };
-  }
+    public async post(endpoint: string, payload: any): Promise<Response> {
+        return await fetch(`${this.options.baseUrl}/${endpoint}`, {
+            method: "POST",
+            headers: this.getHeaders(),
+            body: JSON.stringify(payload),
+        });
+    }
 
-  public async post(endpoint: string, payload: any): Promise<Response> {
-    return await fetch(`${this.options.baseUrl}/${endpoint}`, {
-      method: "POST",
-      headers: this.getHeaders(),
-      body: JSON.stringify(payload),
-    });
-  }
+    public async patch(endpoint: string, payload: any): Promise<Response> {
+        return await fetch(`${this.options.baseUrl}/${endpoint}`, {
+            method: "PATCH",
+            headers: this.getHeaders(),
+            body: JSON.stringify(payload),
+        });
+    }
 
-  public async patch(endpoint: string, payload: any): Promise<Response> {
-    return await fetch(`${this.options.baseUrl}/${endpoint}`, {
-      method: "PATCH",
-      headers: this.getHeaders(),
-      body: JSON.stringify(payload),
-    });
-  }
+    public async get(
+        endpoint: string,
+        params: Record<string, any>
+    ): Promise<Response> {
+        const queryString = new URLSearchParams(params).toString();
+        return await fetch(
+            `${this.options.baseUrl}/${endpoint}?${queryString}`,
+            {
+                method: "GET",
+                headers: this.getHeaders(),
+            }
+        );
+    }
 
-  public async get(endpoint: string, params: Record<string, any>): Promise<Response> {
-    const queryString = new URLSearchParams(params).toString();
-    return await fetch(`${this.options.baseUrl}/${endpoint}?${queryString}`, {
-      method: "GET",
-      headers: this.getHeaders(),
-    });
-  }
-
-  public async delete(endpoint: string): Promise<Response> {
-    return await fetch(`${this.options.baseUrl}/${endpoint}`, {
-      method: "DELETE",
-      headers: this.getHeaders(),
-    });
-  }
+    public async delete(endpoint: string): Promise<Response> {
+        return await fetch(`${this.options.baseUrl}/${endpoint}`, {
+            method: "DELETE",
+            headers: this.getHeaders(),
+        });
+    }
 }
 
 export const sdk: SDK = new SDK({
-  baseUrl: "http://localhost:8080",
+    baseUrl: "http://localhost:8080",
 });
 
 export const isAuthenticated = (): boolean => TokenService.isAuthenticated;
