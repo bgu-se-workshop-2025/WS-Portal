47ae82f3e0285a81e76e9c9a98b87da103cd2a05
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-30 13:23:46 +0300

    completed the jsx and body

--- src/modules/user/admin/components/SuspendUserDialog.tsx
+++ src/modules/user/admin/components/SuspendUserDialog.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import AdminPageDialog from "./AdminPageDialog";
+import { Button, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import { PickerValue } from "@mui/x-date-pickers/internals";
 
 export type SuspendUserDialogProps = {
     openState: {
@@ -10,6 +14,7 @@ export type SuspendUserDialogProps = {
 const SuspendUserDialog = () => {
     const [username, setUsername] = useState("");
     const [period, setPeriod] = useState(0);
+    const [open, setOpen] = useState(true);
 
     const handleSuspend = () => {
         // Logic to suspend the user
@@ -18,4 +23,30 @@ const SuspendUserDialog = () => {
         setUsername("");
         setPeriod(0);
     };
+
+    const handleDateChanged = (value: PickerValue) => {
+        if (!value) {
+            return;
+        }
+        const now = new Date().getMilliseconds();
+        setPeriod(value.getMilliseconds() - now);
+    }
+
+    return <AdminPageDialog
+        title="Suspend User" openState={{
+            open: open,
+            setOpen: setOpen
+        }} >
+        <TextField
+            label="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)} />
+        <DatePicker
+            label="Until?"
+            onChange={handleDateChanged}
+        />
+        <Button onClick={handleSuspend} />
+    </AdminPageDialog>
 }
+
+export default SuspendUserDialog;
