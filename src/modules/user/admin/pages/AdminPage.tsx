fde34a996cb4a6662abf82ab42eae439998db87e
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-30 13:49:20 +0300

    first impl of admin page

--- src/modules/user/admin/pages/AdminPage.tsx
+++ src/modules/user/admin/pages/AdminPage.tsx
@@ -1,5 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+import AdminCommandBar from "../components/AdminCommandBar";
+
 const AdminPage = () => {
-    return <></>
+    return <Stack padding={4}>
+        <Typography variant="h3">System Management</Typography>
+        <AdminCommandBar />
+    </Stack>
 }
 
 export default AdminPage;
