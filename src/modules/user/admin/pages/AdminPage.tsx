7c974a6c4e08dcc1c589d5ae75e4e0d6e8163fa1
Aaron Iziyaev <aron.iz.gab@gmail.com>
2025-05-30 15:36:36 +0300

    completed admin page but refreshing the page doesn't work yet

--- src/modules/user/admin/pages/AdminPage.tsx
+++ src/modules/user/admin/pages/AdminPage.tsx
@@ -1,10 +1,16 @@
 import { Stack, Typography } from "@mui/material";
 import AdminCommandBar from "../components/AdminCommandBar";
+import Resources from "../AdminPageResources.json";
+import SuspendedUsersTable from "../components/SuspendedUsersTable";
 
 const AdminPage = () => {
-    return <Stack padding={4}>
+    return <Stack
+        padding={Resources.Page.LargePadding}
+        gap={Resources.Page.NormalFormGap}
+    >
         <Typography variant="h3">System Management</Typography>
         <AdminCommandBar />
+        <SuspendedUsersTable />
     </Stack>
 }
 
