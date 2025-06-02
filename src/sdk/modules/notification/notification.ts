488f56df8762c5e35271085601b54be263491c44
Noam Argaman <noamarg@post.bgu.ac.il>
2025-06-02 10:06:01 +0300

    added sdk function

--- src/sdk/modules/notification/notification.ts
+++ src/sdk/modules/notification/notification.ts
@@ -1,9 +1,14 @@
 import { Client, IMessage } from "@stomp/stompjs";
+
+import { SDK } from "../../sdk";
+import { Pageable } from "../../../shared/types/dtos";
 import { NotificationPayload } from "../../../shared/types/responses";
 
 const baseurl = "http://localhost:8080/ws";
 const userNotificationsChannel = "/queue/notifications";
 const allUsersNotificationsChannel = "/topic/notifications";
+
+const controller = "notifications";
 
 export const generateClient = () => {
   return new Client({
@@ -20,7 +25,6 @@ export const subscribeToNotifications = (
   username: string | undefined,
   setNotifications: React.Dispatch<React.SetStateAction<NotificationPayload[]>>
 ) => {
-  
   // broadcast to all users
   client.subscribe(allUsersNotificationsChannel, (msg: IMessage) => {
     if (msg.body) {
@@ -29,19 +33,44 @@ export const subscribeToNotifications = (
       setNotifications((prev) => [...prev, notif]);
     }
   });
-  
+
   // user-specific notifications
   if (!username) {
-    console.warn("No username provided, cannot subscribe to user notifications.");
+    console.warn(
+      "No username provided, cannot subscribe to user notifications."
+    );
     return;
   }
 
-  client.subscribe("/user/" + username + userNotificationsChannel, (msg: IMessage) => {
-    console.debug("Received user notification:", msg);
-    if (msg.body) {
-      console.debug("Received user notification:", msg.body);
-      const notif = JSON.parse(msg.body) as NotificationPayload;
-      setNotifications((prev) => [...prev, notif]);
+  client.subscribe(
+    "/user/" + username + userNotificationsChannel,
+    (msg: IMessage) => {
+      console.debug("Received user notification:", msg);
+      if (msg.body) {
+        console.debug("Received user notification:", msg.body);
+        const notif = JSON.parse(msg.body) as NotificationPayload;
+        setNotifications((prev) => [...prev, notif]);
+      }
     }
-  });
+  );
 };
+
+export async function getNotifications(
+  this: SDK,
+  payload: Pageable
+): Promise<NotificationPayload[]> {
+  const params: Record<string, any> = {
+    page: payload.page,
+    size: payload.size,
+  };
+
+  const response = await this.get(controller, params);
+
+  if (!response.ok) {
+    const err = await response.text();
+    throw new Error(`Get notifications failed: ${err}`);
+  }
+
+  const result = (await response.json()) as NotificationPayload[];
+  return result;
+}
