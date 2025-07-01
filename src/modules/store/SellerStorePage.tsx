a0b96d3c25450ebbb06f44361c867d1a5a70172b
Noam Argaman <noamarg@post.bgu.ac.il>
2025-07-01 16:18:30 +0300

    Deleted unnecessary files

--- src/modules/store/SellerStorePage.tsx
+++ src/modules/store/SellerStorePage.tsx
@@ -199,7 +199,7 @@ const SellerStoreLayout: React.FC = () => {
               }}
             />
             <Typography variant="h6" mt={1}>
-              {store.rating > 0 ? `(${store.rating.toFixed(1)})` : ""}
+              {(store.rating ?? 0) > 0 ? `(${store.rating!.toFixed(1)})` : ""}
             </Typography>
           </Box>
         )}
